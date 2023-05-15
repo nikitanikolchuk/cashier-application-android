0c73436042a28f99249d477c8470cf818a24f442
Nikita Nikolchuk <nikolnik@fit.cvut.cz>
2023-05-15 19:06:17 +0200

    Update gitignore and main theme name

--- app/src/main/java/cz/cvut/fit/kot/cashierapplication/MainActivity.kt
+++ app/src/main/java/cz/cvut/fit/kot/cashierapplication/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import cz.cvut.fit.kot.cashierapplication.ui.theme.CashierApplicationTheme
+import cz.cvut.fit.kot.cashierapplication.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CashierApplicationTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    CashierApplicationTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
