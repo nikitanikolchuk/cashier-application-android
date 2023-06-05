01d5dcb98c4644e794afa5436abc128dcbe7c3f3
Nikita Nikolchuk <nikolnik@fit.cvut.cz>
2023-06-05 19:16:48 +0200

    Move OrderInfo to a separate file

---
+++ app/src/main/java/cz/cvut/fit/kot/cashierapplication/ui/component/OrderInfo.kt
@@ -0,0 +1,156 @@
+package cz.cvut.fit.kot.cashierapplication.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import cz.cvut.fit.kot.cashierapplication.R
+import cz.cvut.fit.kot.cashierapplication.data.model.OrderDetailResponseDto
+import cz.cvut.fit.kot.cashierapplication.ui.state.OrderState
+import cz.cvut.fit.kot.cashierapplication.ui.theme.AppTheme
+
+private val sampleOrderDetail = OrderDetailResponseDto(
+    orderId = 1,
+    itemId = 1,
+    name = "item",
+    price = 100,
+    quantity = 1
+)
+private val sampleOrder = OrderState(
+    id = 1,
+    localDateTime = "01.01.23 - 12:00",
+    employeeId = 1,
+    price = 1000,
+    details = List(10) { sampleOrderDetail }
+)
+
+@Composable
+private fun OrderInfoDetail(
+    orderDetail: OrderDetailResponseDto,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.padding(vertical = 8.dp),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(
+            text = "${orderDetail.name} x ${orderDetail.quantity}",
+            modifier = Modifier
+                .weight(0.7f)
+                .padding(start = 25.dp),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1
+        )
+        Text(
+            text = "${orderDetail.price * orderDetail.quantity} ${stringResource(R.string.currency)}",
+            modifier = Modifier.weight(0.3f),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1
+        )
+    }
+}
+
+@Composable
+private fun OrderInfoDetailList(
+    orderDetails: List<OrderDetailResponseDto>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(vertical = 16.dp)
+    ) {
+        items(orderDetails) { detail -> OrderInfoDetail(detail) }
+    }
+}
+
+@Composable
+fun OrderInfo(
+    order: OrderState,
+    onChooseOrder: (OrderState?) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Order #${order.id}",
+            modifier = Modifier
+                .weight(0.2f)
+                .padding(top = 64.dp),
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = order.localDateTime,
+            modifier = Modifier
+                .weight(0.1f)
+                .padding(top = 8.dp),
+        )
+        OrderInfoDetailList(
+            orderDetails = order.details,
+            modifier = Modifier
+                .weight(0.5f)
+                .padding(horizontal = 24.dp)
+        )
+        Text(
+            text = "${stringResource(R.string.total)}: ${order.price} ${stringResource(R.string.currency)}",
+            modifier = Modifier
+                .weight(0.1f)
+                .padding(vertical = 24.dp),
+            fontSize = 24.sp
+        )
+        Button(
+            onClick = { onChooseOrder(null) },
+            modifier = Modifier
+                .weight(0.1f)
+                .width(192.dp)
+                .padding(bottom = 24.dp)
+        ) {
+            Text(stringResource(R.string.back))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OrderInfoDetailPreview() {
+    AppTheme {
+        OrderInfoDetail(sampleOrderDetail)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OrderInfoDetailListPreview() {
+    AppTheme {
+        OrderInfoDetailList(sampleOrder.details)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OrderInfoPreview() {
+    AppTheme {
+        OrderInfo(
+            order = sampleOrder,
+            onChooseOrder = {}
+        )
+    }
+}
--- app/src/main/java/cz/cvut/fit/kot/cashierapplication/ui/state/OrderState.kt
+++ app/src/main/java/cz/cvut/fit/kot/cashierapplication/ui/state/OrderState.kt
@@ -6,17 +6,23 @@ import cz.cvut.fit.kot.cashierapplication.data.model.OrderResponseDto
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 /**
  * Immutable class representing order state with data from API
  */
 data class OrderState(
     val id: Int,
-    val localDateTime: LocalDateTime,
+    val localDateTime: String,
     val employeeId: Int,
     val price: Int,
     val details: List<OrderDetailResponseDto>
 ) {
+    companion object {
+        private val dateTimeFormatter: DateTimeFormatter =
+            DateTimeFormatter.ofPattern("dd.MM.yy - HH:mm")
+    }
+
     /**
      * Datetime from DTO is converted to local using device's time zone ID
      */
@@ -31,7 +37,7 @@ data class OrderState(
                 OrderApi.DATETIME_FORMATTER
             ).toInstant(ZoneOffset.UTC),
             timeZoneId
-        ),
+        ).format(dateTimeFormatter),
         employeeId = orderDto.employeeId,
         price = orderDto.price,
         details = orderDto.details
